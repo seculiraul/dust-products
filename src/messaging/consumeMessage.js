7760189894cfb305b9ea78301ee82bd9880fac56
Raul <raulsmarius@yahoo.com>
2023-08-06 22:32:21 +0300

    Added RabbitMQ Consumer

---
+++ src/messaging/consumeMessage.js
@@ -0,0 +1,25 @@
+const amqp = require('amqplib')
+const rabbitmq_config = require('./rabbitmq_config')
+
+async function consumeMessages() {
+  const connection = await amqp.connect(rabbitmq_config.rabbitmq.url)
+
+  const channel = await connection.createChannel()
+
+  await channel.assertExchange(rabbitmq_config.rabbitmq.exchange, 'direct')
+
+  const q = await channel.assertQueue('orders')
+
+  await channel.bindQueue(
+    q.queue,
+    rabbitmq_config.rabbitmq.exchange,
+    'Order_Created'
+  )
+  channel.consume(q.queue, (msg) => {
+    const data = JSON.parse(msg.content)
+    console.log(data)
+    channel.ack(msg)
+  })
+}
+
+module.exports = consumeMessages
---
+++ src/messaging/rabbitmq_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rabbitmq: {
+    url: 'amqp://localhost',
+    exchange: 'orderExchange',
+  },
+}
